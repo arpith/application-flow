6c1224b86406f7cc1d41859055b65c9f5147d149
Arpith Siromoney <arpith@feedreader.co>
2016-08-30 14:38:03 -0400

    move container from step component to app

--- components/App.jsx
+++ components/App.jsx
@@ -5,11 +5,21 @@ import SubmitButton from './SubmitButton.jsx';
 
 class App extends React.Component {
   render() {
+    const style = {
+      width: '42%',
+      minWidth: '350px',
+      margin: 'auto',
+      padding: '2em',
+      fontFamily: 'gotham, avenir',
+    };
+ 
     return (
       <div>
         <Header />
-        <Step number={4} />
-        <SubmitButton />
+        <div style={style}>
+          <Step number={4} />
+          <SubmitButton />
+        </div>
       </div>
     );
   }
--- components/Step.jsx
+++ components/Step.jsx
@@ -3,20 +3,14 @@ import Logo from './Logo.jsx';
 
 class Step extends React.Component {
   render() {
-    const divStyle = {
-      maxWidth: '10em',
-      margin: 'auto',
-      fontFamily: 'gotham, avenir'
-    };
-    const h1Style = {
+   const style = {
+      fontSize: '1.8em',
       fontWeight: '500',
-      size: '1.4em'
+      paddingBottom: '1em'
     };
     return (
-      <div style={divStyle}>
-        <h1 style={h1Style}>
-          Step {this.props.number}
-        </h1>
+      <div style={style}>
+       Step {this.props.number}
       </div>
     );
   }

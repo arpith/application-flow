ec51aff24703dd8bff394e646b6ab7af1a453025
Arpith Siromoney <arpith@feedreader.co>
2016-09-06 17:35:44 -0400

    use connect (react-redux) for dropdown component

--- app/components/Dropdown.jsx
+++ app/components/Dropdown.jsx
@@ -1,37 +1,36 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import MenuItem from './MenuItem.jsx';
 
-class Dropdown extends React.Component {
-  static contextTypes = {
-    store: PropTypes.any
+function Dropdown({furthest}) {
+  const style = {
+    border: 'thin solid #D7DBDD',
+    borderRadius: '0.2em',
+    boxShadow: '2px 1px 5px #D7DBDD',
+    padding: 0,
+    paddingTop: '0.4em',
+    paddingBottom: '0.4em',
+    margin: 0,
+    width: '8em',
+    position: 'absolute',
+    right: '1em',
+    background: 'white'
   };
-
-  render() {
-    const style = {
-      border: 'thin solid #D7DBDD',
-      borderRadius: '0.2em',
-      boxShadow: '2px 1px 5px #D7DBDD',
-      padding: 0,
-      paddingTop: '0.4em',
-      paddingBottom: '0.4em',
-      margin: 0,
-      width: '8em',
-      position: 'absolute',
-      right: '1em',
-      background: 'white'
-    };
     
-    const enabled = (i) => i < this.context.store.getState().furthest;
-    const items = (i) => <MenuItem key={i} value={`Step ${i+1}`} link={`/step/${i+1}`} enabled={enabled(i)} />;
-    const list = (n) => [...Array(n).keys()].map(items);
+  const enabled = (i) => i < this.context.store.getState().furthest;
+  const items = (i) => <MenuItem key={i} value={`Step ${i+1}`} link={`/step/${i+1}`} enabled={enabled(i)} />;
+  const list = (n) => [...Array(n).keys()].map(items);
 
-    return (
-      <ul style={style}>
-        {list(5)}
-        <MenuItem key='logout' value='Logout' link='/' enabled={true}/>
-      </ul>
-    );
-  }
+  return (
+    <ul style={style}>
+      {list(5)}
+      <MenuItem key='logout' value='Logout' link='/' enabled={true}/>
+    </ul>
+  );
 }
 
-export default Dropdown;
+export default connect(
+  state => ({
+    furthest: state.furthest
+  })
+)(Dropdown);

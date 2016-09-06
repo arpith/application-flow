9a95655e1b9620bffc9d7624e570c346870731ed
Arpith Siromoney <arpith@feedreader.co>
2016-09-06 17:15:24 -0400

    use bcrypt (not bcrypt-node) and redis + promise (setnxAsync)

--- api/signup.js
+++ api/signup.js
@@ -1,17 +1,17 @@
 const redis = require('redis');
 const bluebird = require('bluebird');
-const bcrypt = bluebird.promisifyAll(require('bcrypt-nodejs'));
+const bcrypt = bluebird.promisifyAll(require('bcrypt'));
 bluebird.promisifyAll(redis.RedisClient.prototype);
 
 const client = redis.createClient();
 
 export default (req, res) => {
   bcrypt.genSaltAsync(10).then(function(salt) {
-    return bcrypt.hashAsync(req.params.password, salt, null);
+    return bcrypt.hashAsync(req.body.password, salt);
   }).then(function(hashedPassword) {
-    return client.setnx("auth:"+req.params.username, hashedPassword);
+    return client.setnxAsync("auth:"+req.body.username, hashedPassword);
   }).then(function(resp) {
-    if (resp === "0") {
+    if (resp === 0) {
       res.status(403).json({success: false, error: 'User exists'});
     } else {
       res.json({success: true, message: 'Signup Successful'});

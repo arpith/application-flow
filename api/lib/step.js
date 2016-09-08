eced62b3ccc271c8bb24c944a892b687ceae5f18
Arpith Siromoney <arpith@feedreader.co>
2016-09-08 14:16:58 -0400

    return 1-indexed step when incrementing (redis is 0-indexed)

--- api/lib/step.js
+++ api/lib/step.js
@@ -6,13 +6,12 @@ const client = redis.createClient(process.env.REDIS_URL);
 
 export function increment(username) {
   return client.getAsync('step:'+username).then((step) => {
-    console.log(step)
     if (step < 4) {
       return client.incrAsync('step:'+username);
     } else {
       return step;
     }
-  });
+  }).then(step => parseInt(step) + 1);
 }
 
 export function get(username) {

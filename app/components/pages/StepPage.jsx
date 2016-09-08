8d78096a18f2076cc56aac41b54484c859b587a1
Arpith Siromoney <arpith@feedreader.co>
2016-09-08 15:08:05 -0400

    use react-router-redux push

--- app/components/pages/StepPage.jsx
+++ app/components/pages/StepPage.jsx
@@ -3,14 +3,9 @@ import { connect } from 'react-redux';
 import { push } from 'react-router-redux';
 import Step from '../Step.jsx';
 import Button from '../Button.jsx';
-import {submit} from '../../actions';
+import { submit } from '../../actions';
 
-function StepPage({furthest, step}) {
-  const onSubmit = (e) => {
-    e.preventDefault();
-    dispatch(submit(() => push({}, '/')));
-  };
-
+function StepPage({furthest, step, onSubmit}) {
   return (
     <form onSubmit={onSubmit}>
       <Step number={step} />
@@ -19,9 +14,20 @@ function StepPage({furthest, step}) {
   );
 }
 
-export default connect(
-  (state, ownProps) => ({
+function mapStateToProps(state, ownProps) {
+  return {
     furthest: state.furthest,
-    step: ownProps.params.step
-  })
-)(StepPage);
+    step: ownProps.params.step,
+  };
+}
+
+function mapDispatchToProps(dispatch, {history}) {
+  return {
+    onSubmit: (e) => {
+      e.preventDefault();
+      dispatch(submit(() => dispatch(push('/'))));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(StepPage);
